14cb4cd7ecd1168bb648d8d24e689054552e4858
Sagar Adhikari.blade.php <164398516+Sagarhoraa@users.noreply.github.com>
2025-05-07 21:10:54 +0545

    Add files via upload

---
+++ DOTNET PRACTICE/MyCSharpApp/Program.cs
@@ -0,0 +1,56 @@
+﻿// using System;
+
+// class FactorialCalculator
+// {
+//     public int CalculateFactorial(int number)
+//     {
+//         if (number <= 0) return 1;
+//         return number * CalculateFactorial(number - 1);
+//     }
+// }
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         FactorialCalculator calculator = new FactorialCalculator();
+//         Console.Write("Enter a number: ");
+//         int number = int.Parse(Console.ReadLine());
+//         int factorial = calculator.CalculateFactorial(number);
+//         Console.WriteLine($"Factorial of {number} is {factorial}");
+//     }
+// }
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+//- Write a C# program to calculate sum of array using reference type.
+
+
+
+using System;
+
+class ArraySumCalculator
+{
+    public int CalculateSum(int[] numbers) // Reference type (array)
+    {
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+        return sum;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ArraySumCalculator calculator = new ArraySumCalculator();
+
+        int[] numbers = { 2, 4, 6, 8, 10 }; // Array (Reference Type)
+        int sum = calculator.CalculateSum(numbers);
+        
+        Console.WriteLine("Sum of array elements: " + sum);
+    }
+}
